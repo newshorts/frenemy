19c37aa9f7f6d9968f31a0a4a234787b9e9eec6c
Mike Newell <mike_newell@gspsf.com>
2013-08-28 21:37:30 -0700

    removing needless dependencies

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-var async   = require('async');
+//var async   = require('async');
 var express = require('express');
 var util    = require('util');
 
@@ -9,12 +9,12 @@ var app = express.createServer(
   express.bodyParser(),
   express.cookieParser(),
   // set this to a secret value to encrypt session cookies
-  express.session({ secret: process.env.SESSION_SECRET || 'secret123' }),
-  require('faceplate').middleware({
-    app_id: process.env.FACEBOOK_APP_ID,
-    secret: process.env.FACEBOOK_SECRET,
-    scope:  'user_likes,user_photos,user_photo_video_tags'
-  })
+  express.session({ secret: process.env.SESSION_SECRET || 'secret123' })
+//  require('faceplate').middleware({
+//    app_id: process.env.FACEBOOK_APP_ID,
+//    secret: process.env.FACEBOOK_SECRET,
+//    scope:  'user_likes,user_photos,user_photo_video_tags'
+//  })
 );
 
 // listen to the PORT given to us in the environment
@@ -94,44 +94,44 @@ function handle_translator_request(req, res) {
 function handle_facebook_request(req, res) {
     
   // if the user is logged in
-  if (req.facebook.token) {
-
-    async.parallel([
-      function(cb) {
-        // query 4 friends and send them to the socket for this socket id
-        req.facebook.get('/me/friends', { limit: 4 }, function(friends) {
-          req.friends = friends;
-          cb();
-        });
-      },
-      function(cb) {
-        // query 16 photos and send them to the socket for this socket id
-        req.facebook.get('/me/photos', { limit: 16 }, function(photos) {
-          req.photos = photos;
-          cb();
-        });
-      },
-      function(cb) {
-        // query 4 likes and send them to the socket for this socket id
-        req.facebook.get('/me/likes', { limit: 4 }, function(likes) {
-          req.likes = likes;
-          cb();
-        });
-      },
-      function(cb) {
-        // use fql to get a list of my friends that are using this app
-        req.facebook.fql('SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1', function(result) {
-          req.friends_using_app = result;
-          cb();
-        });
-      }
-    ], function() { // callback
-      render_page(req, res);
-    });
-
-  } else {
+//  if (req.facebook.token) {
+//
+//    async.parallel([
+//      function(cb) {
+//        // query 4 friends and send them to the socket for this socket id
+//        req.facebook.get('/me/friends', { limit: 4 }, function(friends) {
+//          req.friends = friends;
+//          cb();
+//        });
+//      },
+//      function(cb) {
+//        // query 16 photos and send them to the socket for this socket id
+//        req.facebook.get('/me/photos', { limit: 16 }, function(photos) {
+//          req.photos = photos;
+//          cb();
+//        });
+//      },
+//      function(cb) {
+//        // query 4 likes and send them to the socket for this socket id
+//        req.facebook.get('/me/likes', { limit: 4 }, function(likes) {
+//          req.likes = likes;
+//          cb();
+//        });
+//      },
+//      function(cb) {
+//        // use fql to get a list of my friends that are using this app
+//        req.facebook.fql('SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1', function(result) {
+//          req.friends_using_app = result;
+//          cb();
+//        });
+//      }
+//    ], function() { // callback
+//      render_page(req, res);
+//    });
+//
+//  } else {
     render_page(req, res);
-  }
+//  }
   
 }
 
